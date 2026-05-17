6749ffffeb194f3d10852d9ee6025a61741958c7
grevian <grevian@gmail.com>
2026-05-17 22:49:12 +0100

    push out of the 1a AZ where there's no spot availability

--- terraform/network.tf
+++ terraform/network.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index+1]
   map_public_ip_on_launch = true
 
   tags = { Name = "${local.name_prefix}-public-${count.index}" }
